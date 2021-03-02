88d31becc73d610c6cdba764cfed7e76150b1322
jordles113 <jordan.t.romero@gmail.com>
2021-03-02 12:43:35 -0600

    Completes challenge

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        double num1 = 20.00d; 
+        double num2 = 80.00d; 
+        double result = (num1 + num2) * 100.00; 
+        result = result % 40.00; 
+        boolean bool = result == 0 ? true : false; 
+        System.out.println(bool);
+
+        if(bool == false) {
+            System.out.println("Got some remainder");
+        }
+    }
+}
